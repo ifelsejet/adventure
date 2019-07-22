5722e878e5fb2a17153eea771d0ec7ca4394c4d6
Jatnael Montes <ifelsejet@gmail.com>
2019-07-22 16:18:23 -0700

    Started branching the adventure and made options for the RUN pathway

--- main.py
+++ main.py
@@ -9,7 +9,39 @@ class MainHandler(webapp2.RequestHandler):
         template = env.get_template("templates/start.html")
         self.response.write(template.render())
 
+class RunHandler(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/run.html")
+        self.response.write(template.render())
+
+class TJayHandler(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/tjay.html")
+        self.response.write(template.render())
+
+class HideHandler(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/hide.html")
+        self.response.write(template.render())
+
+class RunFasterHandler(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/runFaster.html")
+        self.response.write(template.render())
+
+class EnterHandler(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/enter.html")
+        self.response.write(template.render())
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    
+    ('/run', RunHandler),
+    ('/tjay', TJayHandler),
+    ('/hide', HideHandler),
+    ('/runFaster', RunFasterHandler),
+    ('/enter', EnterHandler),
+
+
 ], debug = True)
